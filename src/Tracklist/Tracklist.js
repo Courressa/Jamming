51373dee06a3098c4f74a6e347557b88076b57c8
Courressa <courressamalcolm2030@gmail.com>
2024-09-18 21:54:08 -0400

    add style

--- src/Tracklist/Tracklist.js
+++ src/Tracklist/Tracklist.js
@@ -3,7 +3,7 @@ import styles from "./Tracklist.module.css";
 
 function Tracklist(props) {
   return (
-    <div className={styles.div}>
+    <div className={styles.eachSong}>
       <h2>{props.songObject.name}</h2>
       <h3>{props.songObject.artist}</h3>
       {props.children}
